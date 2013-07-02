8e2f9f77f95a1aa5c84b3a3d70d29d709fa317db
zgs225 <zgs225@gmail.com>
2013-07-02 20:26:25 +0800

    validate and units test

--- test/helpers/products_helper_test.rb
+++ test/helpers/products_helper_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class ProductsHelperTest < ActionView::TestCase
+    test "product attributes must not be empty" do
+        product = Product.new
+        assert product.invalid? 
+        assert product.errors[:title].any?
+        assert product.errors[:description].any?
+        assert product.errors[:price].any?
+        assert product.errors[:image_url].any?
+    end
 end
