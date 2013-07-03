2936cd63ac451c1f88d4f44074dae10e85de0d13
zgs225 <zgs225@gmail.com>
2013-07-03 22:12:06 +0800

    fixtures and models units test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    fixtures :products
+
+    test "product is not valid without a unique title" do
+        product = Product.new(:title => products(:ruby).title,
+                              :description => "yyy",
+                              :price => 1,
+                              :image_url => "fred.gif")
+        assert !product.save
+        assert_equal "has already been taken", product.errors[:title].join('; ')
+    end
+
+    test "product is not valid without a unique title - i18n" do
+        product = Product.new(:title => products(:ruby).title,
+                              :description => "yyy",
+                              :price => 1,
+                              :image_url => "fred.gif")
+        assert !product.save
+        assert_equal I18n.translate('activerecord.errors.messages.taken'),
+            product.errors[:title].join('; ')
+    end
 end
